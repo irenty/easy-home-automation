6dfabd7a7813f0c567affd42c64c97db196cc39f
Irek <irek.marciniak@gmail.com>
2017-06-13 02:44:26 +0100

    server deploy script

--- rasp-server/deploy.sh
+++ rasp-server/deploy.sh
@@ -7,6 +7,12 @@ echo 'Creating rasp-server folder'
 rm -rvf $server_folder
 mkdir -p $server_folder
 
+echo 'Copying server files'
 cp ./server.py $server_folder
+
+echo 'Copying dummy lirc driver'
+cp ./lirc_driver.py $server_folder
+
+echo 'Copying real lwrf driver'
 cp ./../lightwave-rf/lwrfCustom.py $server_folder
 cp ./../lightwave-rf/lwrf_driver.py $server_folder
